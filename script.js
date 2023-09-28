4ad42c4f0c90bbcd3ae34b6df21c7f9c669170c9
nineInchClous <thibault.castelli@pm.me>
2023-09-28 15:29:45 -0400

    Add buttons functions

--- script.js
+++ script.js
@@ -1,60 +1,88 @@
+// Global variables
 let firstNumber = 0;
-const operator = '';
+let operator = '';
 let secondNumber = 0;
+let strDisplay = '';
+let addToFirstNumber = true;
 
 const display = document.getElementById('display');
 
+const divideOperator = document.getElementById('/');
+const multiplyOperator = document.getElementById('*');
+const minusOperator = document.getElementById('-');
+const plusOperator = document.getElementById('+');
+
 // Events
-document.getElementById('0').addEventListener('click', () => {
-  printOperation();
-});
-document.getElementById('1').addEventListener('click', () => {
-  firstNumber += 1;
-  printOperation();
-});
-document.getElementById('2').addEventListener('click', () => {
-  firstNumber += 2;
-  printOperation();
-});
-document.getElementById('3').addEventListener('click', () => {
-  firstNumber += 3;
-  printOperation();
-});
-document.getElementById('4').addEventListener('click', () => {
-  firstNumber += 4;
-  printOperation();
-});
-document.getElementById('5').addEventListener('click', () => {
-  firstNumber += 5;
-  printOperation();
-});
-document.getElementById('6').addEventListener('click', () => {
-  firstNumber += 6;
-  printOperation();
-});
-document.getElementById('7').addEventListener('click', () => {
-  firstNumber += 7;
-  printOperation();
-});
-document.getElementById('8').addEventListener('click', () => {
-  firstNumber += 8;
-  printOperation();
-});
-document.getElementById('9').addEventListener('click', () => {
-  firstNumber += 9;
-  printOperation();
+document.getElementById('0').addEventListener('click', () => numberClick(0));
+document.getElementById('1').addEventListener('click', () => numberClick(1));
+document.getElementById('2').addEventListener('click', () => numberClick(2));
+document.getElementById('3').addEventListener('click', () => numberClick(3));
+document.getElementById('4').addEventListener('click', () => numberClick(4));
+document.getElementById('5').addEventListener('click', () => numberClick(5));
+document.getElementById('6').addEventListener('click', () => numberClick(6));
+document.getElementById('7').addEventListener('click', () => numberClick(7));
+document.getElementById('8').addEventListener('click', () => numberClick(8));
+document.getElementById('9').addEventListener('click', () => numberClick(9));
+
+divideOperator.addEventListener('click', () => operatorClick('/'));
+multiplyOperator.addEventListener('click', () => operatorClick('*'));
+minusOperator.addEventListener('click', () => operatorClick('-'));
+plusOperator.addEventListener('click', () => operatorClick('+'));
+
+document.getElementById('=').addEventListener('click', () => {
+  const result = operate(firstNumber, secondNumber, operator);
+  display.textContent = result;
+  resetGlobalVariables();
 });
 
-function btnClick(btnValue) {
-  if (!operator) {
-    firstNumber += btnValue;
-  } else {
-    secondNumber += btnValue;
-  }
-  printOperation();
+document.getElementById('clear').addEventListener('click', () => {
+  resetGlobalVariables();
+  display.textContent = strDisplay;
+});
+
+/**
+ * Reset all global variables
+ */
+function resetGlobalVariables() {
+  firstNumber = 0;
+  secondNumber = 0;
+  operator = '';
+  strDisplay = '';
+  addToFirstNumber = true;
 }
-function printOperation() {
-  display.textContent = firstNumber + operator + secondNumber;
+
+/**
+ * Add the value of the clicked button to the operator variable
+ * @param {string} operatorValue Value of the clicked button
+ */
+function operatorClick(operatorValue) {
+  if (operator) {
+    const result = operate(firstNumber, secondNumber, operator);
+    firstNumber = result;
+    secondNumber = 0;
+    strDisplay = result.toString();
+  } 
+
+  operator = operatorValue;
+  strDisplay += operator;
+  display.textContent = strDisplay;
+
+  addToFirstNumber = false;
+}
+
+/**
+ * Add the value of the clicked button
+ * @param {number} numberValue Value of the clicked button
+ */
+function numberClick(numberValue) {
+  strDisplay += numberValue;
+  display.textContent = strDisplay;
+
+  if (addToFirstNumber) {
+    firstNumber = parseInt(strDisplay);
+  } else {
+    secondNumber = parseInt(strDisplay.substring(strDisplay.indexOf(operator) + 1));
+  }
 }
 
 /**
