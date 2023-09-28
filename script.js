1576593e2f4d9919308be41bda0481e97999d299
nineInchClous <thibault.castelli@pm.me>
2023-09-28 11:52:18 -0400

    Add eslint

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = 0;
+const operator = '';
+let secondNumber = 0;
+
+const display = document.getElementById('display');
+
+// Events
+document.getElementById('0').addEventListener('click', () => {
+  printOperation();
+});
+document.getElementById('1').addEventListener('click', () => {
+  firstNumber += 1;
+  printOperation();
+});
+document.getElementById('2').addEventListener('click', () => {
+  firstNumber += 2;
+  printOperation();
+});
+document.getElementById('3').addEventListener('click', () => {
+  firstNumber += 3;
+  printOperation();
+});
+document.getElementById('4').addEventListener('click', () => {
+  firstNumber += 4;
+  printOperation();
+});
+document.getElementById('5').addEventListener('click', () => {
+  firstNumber += 5;
+  printOperation();
+});
+document.getElementById('6').addEventListener('click', () => {
+  firstNumber += 6;
+  printOperation();
+});
+document.getElementById('7').addEventListener('click', () => {
+  firstNumber += 7;
+  printOperation();
+});
+document.getElementById('8').addEventListener('click', () => {
+  firstNumber += 8;
+  printOperation();
+});
+document.getElementById('9').addEventListener('click', () => {
+  firstNumber += 9;
+  printOperation();
+});
+
+function btnClick(btnValue) {
+  if (!operator) {
+    firstNumber += btnValue;
+  } else {
+    secondNumber += btnValue;
+  }
+  printOperation();
+}
+function printOperation() {
+  display.textContent = firstNumber + operator + secondNumber;
+}
 
 /**
- * Do a mathematical operation between a and b based on the operator 
+ * Do a mathematical operation between a and b based on the operator
  * (sum, subtraction, multiplication or division)
  * @param {number} a First number
  * @param {number} b Second number
